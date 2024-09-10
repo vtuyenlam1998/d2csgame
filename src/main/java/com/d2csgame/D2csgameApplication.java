92f575cc335b8f3152725edbea50906322b17fff
PC12\Admin <vtuyenlam1998@gmail.com>
2024-09-10 09:07:04 +0700

    first commit

---
+++ src/main/java/com/d2csgame/D2csgameApplication.java
@@ -0,0 +1,13 @@
+package com.d2csgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2csgameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(D2csgameApplication.class, args);
+    }
+
+}
